fd74318d5faa0fcda9adb5154a1ee333c2332b33
Aleksei Gurylev <gurylevalexey@gmail.com>
2024-09-14 23:37:10 +0300

    Animated Circular Saw added, Moat done.

---
+++ Source/CarCombat/Hazards/CircularSaw.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CircularSaw.h"
+
+// Sets default values
+ACircularSaw::ACircularSaw()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SawMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Saw Mesh");
+	RootComponent = SawMesh;
+
+	Collider = CreateDefaultSubobject<UStaticMeshComponent>("Collider");
+	Collider->SetupAttachment(SawMesh);
+	Collider->SetVisibility(false);
+	Collider->OnComponentHit.AddDynamic(this, &ACircularSaw::OnColliderOverlap);
+}
+
+// Called when the game starts or when spawned
+void ACircularSaw::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SawMesh->PlayAnimation(RotationAnim, true);
+}
+
+// Called every frame
+void ACircularSaw::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACircularSaw::OnColliderOverlap(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	
+}
